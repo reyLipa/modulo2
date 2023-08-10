bbdf2f0343023a9b9b2f2e880765b2c25e760c53
reyLipa <109700575+reyLipa@users.noreply.github.com>
2023-08-10 20:10:19 -0300

    Initial commit

---
+++ src/modulo2/Modulo2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulo2;
+
+/**
+ *
+ * @author Isabel
+ */
+public class Modulo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
